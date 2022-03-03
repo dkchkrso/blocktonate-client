cd549b9d62667d2bc0e5ed6d3745ca777a22068d
Christian Kroer Sørensen <dkchkrso@lego.com>
2022-03-03 13:10:32 +0100

    WalletCard created

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import ReceiverListPage from "./pages/ReceiverListPage";
+import WalletCard from "./components/WalletCard";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-
+      <WalletCard />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/receivers" element={<ReceiverListPage />} />
